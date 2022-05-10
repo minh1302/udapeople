005833142a7f630a9ad435fcf6c2833898cf78c1
thoangminh <truonghoangminh1302@gmail.com>
2022-05-10 12:13:33 +0700

    modify src

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,12 +27,11 @@ async function bootstrap() {
     .setDescription('Glee2 API')
     .setVersion('1.0')
     .addTag('customTag')
-    .setBasePath(apiVersionPrefix)
-    .addBearerAuth()//x // here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
-  const config: ConfigService = app.get('ConfigService');
+  const config: ConfigService = app.get(ConfigService);
   const whitelist = config.CORS_WHITELIST;
   const corsOptions = {
     origin(origin, callback) {
